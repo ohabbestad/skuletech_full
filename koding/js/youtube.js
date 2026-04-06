1e0e5c485682bfcabd187c6c5f5822c3ad38bc19
ohabbestad <ole.habbestad@gmail.com>
2026-04-06 20:29:46 +0000

    Stopp av YT

--- koding/js/youtube.js
+++ koding/js/youtube.js
@@ -62,4 +62,30 @@
       div.innerHTML = id ? byggEmbed(id) : plasshalderbanner;
     });
   });
+
+  // Stopp alle YouTube-videoer når brukaren navigerer vekk frå sida
+  function stoppAlleYouTubeVideoer() {
+    var iframes = document.querySelectorAll('iframe[src*="youtube.com/embed"]');
+    iframes.forEach(function (iframe) {
+      try {
+        iframe.contentWindow.postMessage('{"event":"command","func":"stopVideo","args":""}', '*');
+      } catch (e) {
+        // Ignorer feil, kanskje iframe ikkje er lasta enno
+      }
+    });
+  }
+
+  // Stopp videoer når sida lastar ut (navigasjon vekk)
+  window.addEventListener('beforeunload', stoppAlleYouTubeVideoer);
+  window.addEventListener('pagehide', stoppAlleYouTubeVideoer);
+
+  // Stopp videoer når sida blir skjult (f.eks. bytt tab)
+  document.addEventListener('visibilitychange', function () {
+    if (document.hidden) {
+      stoppAlleYouTubeVideoer();
+    }
+  });
+
+  // Eksporter funksjonen for manuell bruk om nødvendig
+  window.stoppYouTubeVideoer = stoppAlleYouTubeVideoer;
 })();
